6f94fc37bdb28c05a96cee02107dc00649e8c90e
saylesss88 <saylesss87@proton.me>
2025-04-30 09:37:38 -0400

    nh flake dir

--- home/nh.nix
+++ home/nh.nix
@@ -1,11 +1,11 @@
-{userVars, ...}: {
+_: {
   programs = {
     nh = {
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 4d --keep 5";
-      # flake = "/home/jr/flake";
-      flake = userVars.flake;
+      flake = "/home/jr/flake";
+      # flake = userVars.flake;
     };
   };
 }
