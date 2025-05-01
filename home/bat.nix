e105a4caafa4d493a81eed2383b9c9eed2b75104
saylesss88 <saylesss87@proton.me>
2025-05-01 11:17:28 -0400

    batModule

--- home/bat.nix
+++ home/bat.nix
@@ -12,24 +12,26 @@ in {
     description = "Enable bat module";
   };
 
-  programs.bat = lib.mkIf cfg.enable {
-    enable = true;
-    themes = {
-      dracula = {
-        src = pkgs.fetchFromGitHub {
-          owner = "dracula";
-          repo = "sublime"; # Bat uses sublime syntax for its themes
-          rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
-          sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+  config = lib.mkIf cfg.enable {
+    programs.bat = {
+      enable = true;
+      themes = {
+        dracula = {
+          src = pkgs.fetchFromGitHub {
+            owner = "dracula";
+            repo = "sublime"; # Bat uses sublime syntax for its themes
+            rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+            sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+          };
+          file = "Dracula.tmTheme";
         };
-        file = "Dracula.tmTheme";
       };
+      extraPackages = with pkgs.bat-extras; [
+        batdiff
+        batman
+        prettybat
+        batgrep
+      ];
     };
-    extraPackages = with pkgs.bat-extras; [
-      batdiff
-      batman
-      prettybat
-      batgrep
-    ];
   };
 }
