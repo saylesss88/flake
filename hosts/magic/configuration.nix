404253fd86fad9ecb6fca0cdd673ea869cb0984c
saylesss88 <saylesss87@proton.me>
2025-05-18 15:59:58 -0400

    home

--- hosts/magic/configuration.nix
+++ hosts/magic/configuration.nix
@@ -20,7 +20,7 @@
 
   # Home-Manager Configuration needs to be here for home.packages to be available in the Configuration Package and VM i.e. `nix build .#nixos`
   home-manager = {
-    # useGlobalPkgs = true;
+    useGlobalPkgs = true;
     useUserPackages = true;
     extraSpecialArgs = {inherit pkgs inputs host system colorscheme userVars;};
     users.jr = {...}: {
