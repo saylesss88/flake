63f83a2ed73ce7b2662dca95c335b3d74ce7b014
saylesss88 <saylesss87@proton.me>
2025-05-23 12:10:25 -0400

    bash aliases

--- home/shells/bash.nix
+++ home/shells/bash.nix
@@ -29,9 +29,9 @@ _: {
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       v = "nvim";
       cat = "bat";
-      ls = "eza --icons";
-      ll = "eza -lh --icons --grid --group-directories-first";
-      la = "eza -lah --icons --grid --group-directories-first";
+      # ls = "eza --icons";
+      # ll = "eza -lh --icons --grid --group-directories-first";
+      # la = "eza -lah --icons --grid --group-directories-first";
       yz = "yazi";
       ".." = "cd ..";
       "..." = "cd ../..";
