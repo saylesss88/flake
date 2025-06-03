686d211da7c0ee64b40fc5a01599ed7e1cd05926
saylesss88 <saylesss87@proton.me>
2025-06-03 19:59:31 -0400

    update

--- hosts/magic/configuration.nix
+++ hosts/magic/configuration.nix
@@ -12,17 +12,18 @@
     ./hardware.nix
     ./security.nix
     ./users.nix
-    # inputs.lib.nixOsModules # Likely redundant, remove if it causes issues
+    inputs.lib.nixOsModules
+    # inputs.nixos-hardware.nixosModules.common-gpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd
+    # inputs.stylix.nixosModules.stylix
     inputs.home-manager.nixosModules.home-manager
     inputs.disko.nixosModules.default
-    # inputs.impermanence.nixosModules.impermanence # Only keep if you use it for other paths like /var/log, etc.
+    inputs.impermanence.nixosModules.impermanence
     ./disk-config.nix
-    ./impermanence.nix # If this file defines additional impermanence-related configurations
+    ./impermanence.nix
   ];
 
-  # Home-Manager Configuration needs to be here for home.packages to be available in the Configuration Package and VM i.e.
-  # `nix build .#nixos`
+  # Home-Manager Configuration needs to be here for home.packages to be available in the Configuration Package and VM i.e. `nix build .#nixos`
   home-manager = {
     useUserPackages = true;
     extraSpecialArgs = {inherit pkgs inputs host system colorscheme userVars;};
@@ -44,9 +45,9 @@
     inputs.lib.overlays
   ];
 
-  # disko.enable = true; # Not needed if disko is imported via inputs.disko.nixosModules.default
+  # disko.enable = true;
 
-  # Enable/Disable Custom Modules
+  #  Enable/Disable Custom Modules
   custom = {
     # stylixModule.enable = true;
     bootModule.enable = true;
@@ -69,40 +70,40 @@
   programs.nix-ld.libraries = [];
   # virtualisation.libvirtd.enable = true;
   # virtualisation.qemu.package = pkgs.qemu_kvm;
-  # virtualisation.qemu.options = ["-enable-kvm"]; # Correct syntax
+  # virtualisation.qemu.options = ["-enable-kvm"];
   # virtualisation.vmVariant = "kvm"; # Optimized for KVM
-  # local.hardware-clock.enable = true; # If local.hardware-clock is a custom option, make sure it's defined
+  # local.hardware-clock.enable = true;
 
-  # During system activation, compare the closure size difference between the current and new system and display a formatted
-  # table if significant changes are detected.
+  # During system activation, compare the closure size difference between the current and new system and display a formatted table if significant changes are detected.
   system.activationScripts.diff = ''
     if [[ -e /run/current-system ]]; then
-      ${pkgs.nushell}/bin/nu -c "let diff_closure = (${pkgs.nix}/bin/nix store diff-closures /run/current-system '$systemConfig'); let table = ($diff_closure | lines | where $it =~ KiB | where $it =~ → | parse -r '^(?<Package>\S+): (?<Old>[^,]+)(?:.*) → (?<New>[^,]+)(?:.*), (?<DiffBin>.*)$' | insert Diff { get DiffBin | ansi strip | into filesize } | sort-by -r Diff | reject DiffBin); if ($table | get Diff | is-not-empty) { print \"\"; $table | append [[Package Old New Diff]; [\"\" \"\" \"\" \"\"]] | append [[Package Old New Diff]; [\"\" \"\" \"Total:\" ($table | get Diff | math sum) ]] | print; print \"\" }"
+      ${pkgs.nushell}/bin/nu -c "let diff_closure = (${pkgs.nix}/bin/nix store diff-closures /run/current-system '$systemConfig'); let table = (\$diff_closure | lines | where \$it =~ KiB | where \$it =~ → | parse -r '^(?<Package>\S+): (?<Old>[^,]+)(?:.*) → (?<New>[^,]+)(?:.*), (?<DiffBin>.*)$' | insert Diff { get DiffBin | ansi strip | into filesize } | sort-by -r Diff | reject DiffBin); if (\$table | get Diff | is-not-empty) { print \"\"; \$table | append [[Package Old New Diff]; [\"\" \"\" \"\" \"\"]] | append [[Package Old New Diff]; [\"\" \"\" \"Total:\" (\$table | get Diff | math sum) ]] | print; print \"\" }"
     fi
   '';
 
-  # Enable Impermanence isEphemeral = true; # This line is commented out.
-  # If you want to use the nixos-impermanence module for '/', uncomment this and remove the manual script.
-  # fileSystems."/".options = ["${inputs.impermanence.fileSystemOptions.ephemeral}"]; # Example for impermanence module
+  # Enable Impermanence
+  # isEphemeral = true;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # nixpkgs.config.permittedInsecurePackages = ["olm-3.2.16"]; # Uncomment if needed
+  # nixpkgs.config.permittedInsecurePackages = ["olm-3.2.16"];
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
-  # system.autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable"; # Uncomment if you want unstable channel
+  # system.autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable";
 
   # console.keyMap = userVars.keys;
   console.keyMap = "us";
 
-  # nixpkgs.config.allowUnfree = true; # Remove this if you only want to allow specified packages via allowUnfreePredicate
+  nixpkgs.config.allowUnfree = true;
 
   networking.hostName = "magic";
-  # This value determines the NixOS release from which the default settings for stateful data, like file locations and
-  # database versions on your system were taken. It‘s perfectly fine and recommended to leave this value at the release
-  # version of the first install of this system. Before changing this value read the documentation for this option (e.g. man
-  # configuration.nix or on https://nixos.org/nixos/options.html).
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 }
