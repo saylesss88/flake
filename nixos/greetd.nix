d14ccab9972d681c6b2f184eb057cdfe7701866c
saylesss88 <saylesss87@proton.me>
2025-07-01 18:46:24 -0400

    greetd

--- nixos/greetd.nix
+++ nixos/greetd.nix
@@ -1,11 +1,12 @@
 {pkgs, ...}: {
   services.greetd = {
     enable = true;
-    settings = {
-      default_session = {
-        user = "greeter";
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = "jr";
       };
+      default_session = initial_session;
     };
   };
 }
