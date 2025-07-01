f7fc9e511af617a42935532e5c7b0ff86fea27fc
saylesss88 <saylesss87@proton.me>
2025-07-01 17:51:24 -0400

    greetd refactor

--- nixos/greetd.nix
+++ nixos/greetd.nix
@@ -1,12 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  username,
+  ...
+}: {
   services.greetd = {
     enable = true;
-    settings = rec {
-      initial_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland";
-        user = "jr";
+    vt = 3;
+    settings = {
+      default_session = {
+        user = username;
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
       };
-      default_session = initial_session;
     };
   };
 }
