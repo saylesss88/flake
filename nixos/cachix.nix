0615886f3cb5788eb52a1c778efdcbb2f547ccb3
saylesss88 <saylesss87@proton.me>
2025-07-03 13:04:14 -0400

    cachix.nix

--- nixos/cachix.nix
+++ nixos/cachix.nix
@@ -14,8 +14,10 @@ in {
     environment.systemPackages = with pkgs; [cachix];
 
     nix.extraOptions = "gc-keep-outputs = true";
+
     nix.settings = {
       substituters = [
+        "https://cache.nixos.org/" # <--- ADD THIS BACK!
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://ghostty.cachix.org"
@@ -28,6 +30,7 @@ in {
         # "https://nixpkgs-wayland.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" # <--- ADD THIS KEY!
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
