c8c72d827f476288a4cf34651da4c8886b9d7301
saylesss88 <saylesss87@proton.me>
2026-03-01 15:43:55 +0000

    imperm rollback script adjustments

--- hosts/magic/impermanence.nix
+++ hosts/magic/impermanence.nix
@@ -5,8 +5,14 @@
 }: {
   imports = [inputs.impermanence.nixosModules.impermanence];
   boot.initrd.postDeviceCommands = lib.mkAfter ''
+    # 1. Wait for LUKS
+    udevadm settle
+    # 2. Force the pool into the "garage"
     zpool import -f -N rpool
+    # 3. Clean the slate
     zfs rollback -r rpool/local/root@blank
+    # 4. Give the pool back to the system
+    zpool export rpool
   '';
 
   environment.persistence."/persist" = {
