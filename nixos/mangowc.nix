0099360272e8e618e0737ab6afdedf18c53de5a7
saylesss88 <saylesss87@proton.me>
2026-03-02 16:25:05 +0000

    move to autostart_sh

--- nixos/mangowc.nix
+++ nixos/mangowc.nix
@@ -400,19 +400,19 @@ in
       };
 
       home.file = {
-        ".config/mango/autostart.sh".source = pkgs.writers.writeBash "autostart.sh" ''
-          # wl-paste --type text --watch cliphist store &
-          # Keep clipboard content after app closes
-          wl-clip-persist --clipboard regular --reconnect-tries 0 &
-          # Watch clipboard and store history
-          wl-paste --type text --watch cliphist store &
-          wl-paste --type image --watch cliphist store &
-          mako &
-          dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-          # kanshi &
-          waybar &
-          wpaperd &
-        '';
+        # ".config/mango/autostart.sh".source = pkgs.writers.writeBash "autostart.sh" ''
+        #   # wl-paste --type text --watch cliphist store &
+        #   # Keep clipboard content after app closes
+        #   wl-clip-persist --clipboard regular --reconnect-tries 0 &
+        #   # Watch clipboard and store history
+        #   wl-paste --type text --watch cliphist store &
+        #   wl-paste --type image --watch cliphist store &
+        #   mako &
+        #   dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+        #   # kanshi &
+        #   waybar &
+        #   wpaperd &
+        # '';
         "Pictures/Wallpapers".source = inputs.wallpapers;
         ".config/wpaperd/config.toml".text = ''
           [default]
